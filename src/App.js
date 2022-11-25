d2fe1353d1129f1418952e787d790308dd01f523
latin bubel <esterapietkiewicz0110@gmail.com>
2022-11-25 11:23:50 +0100

    added bootrstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Estera Pietkiewicz and it is{" "}
-        <a
-          href="https://github.com/latin-bubel/react-weather-app-course"
-          target="_blank"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by Estera Pietkiewicz and it is{" "}
+          <a
+            href="https://github.com/latin-bubel/react-weather-app-course"
+            target="_blank"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
