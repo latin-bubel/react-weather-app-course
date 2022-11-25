b550418d1d15b695ae3db387107c8e8183cf6332
latin bubel <esterapietkiewicz0110@gmail.com>
2022-11-25 12:41:31 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is created by Estera Pietkiewicz and it is{" "}
           <a
