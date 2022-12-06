61f478d1895e0dc89aafddb9af804438f198fc10
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 15:13:34 +0100

    Updated WeatherForecastDay
    Inluded name of days (array)
    Added functions to mathround temp

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,16 +2,34 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemp = Math.round(props.data.temperature.maximum);
+    return `${maxTemp}°`;
+  }
+
+  function minTemperature() {
+    let minTemp = Math.round(props.data.temperature.minimum);
+    return `${minTemp}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="WeatherForecast-day"> {props.data.time}</div>
+      <div className="WeatherForecast-day"> {day()}</div>
       <WeatherIcon code={props.data.condition.icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {props.data.temperature.maximum}°
+          {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
-          {props.data.temperature.minimum}°
+          {minTemperature()}
         </span>
       </div>
     </div>
