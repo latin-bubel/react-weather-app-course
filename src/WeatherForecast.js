91419d44f8069556201a4f11dc1585bd1c1268b5
latin bubel <esterapietkiewicz0110@gmail.com>
2022-12-06 13:20:03 +0100

    Included weather icon for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./WeatherForecast.css";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast() {
   return (
@@ -7,7 +8,7 @@ export default function WeatherForecast() {
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
-
+          <WeatherIcon code="clear-sky-day" size={36} />
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
